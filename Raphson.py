091873f6cde92d286fa3c705a131b648e8a106f0
Katarisu <114144185+JerichoThe@users.noreply.github.com>
2023-06-05 20:47:02 +0700

    Raphson

--- Raphson.py
+++ Raphson.py
@@ -1,19 +1,22 @@
 import numpy as np
-
 def f(x):
-  return x**2 - 5
+	return 2*x**2 + 8*x - 5
 
 def g(x):
-  return 2*x
+	return 4*x + 8
 
-def raphson(x0, e):
-  x1 = x0 - f(x0)/g(x0)
+def raphson(x0,e=0.01,t=50):
 
-  if(np.abs(f(x1)) < e):
-    print("Root = ", x1)
-    return
+	if(np.abs(f(x0)) < e):
+		print("Root = ", x0)
+		return
 
-  raphson(x1,e)
+	x1 = x0 - f(x0)/g(x0)
+	raphson(x1)
+    
+raphson(6)
+# Max Iterate : 50
 
-raphson(2, 0.1)
-raphson(12, 0.1)
+# Tolerance : 0.01
+
+# First Prediction = 6
