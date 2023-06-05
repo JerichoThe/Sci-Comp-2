a92b8b5512859b5ae0192966a09954f587ca4a1b
Katarisu <114144185+JerichoThe@users.noreply.github.com>
2023-06-05 20:45:39 +0700

    Linear Square Regression

--- LSR.py
+++ LSR.py
@@ -1,17 +1,18 @@
+x = [0,1,2,3,4,5,6,7,8,9]
+y = [4,2,9,4,2,6,9,3,7,10]
 import matplotlib.pyplot as plt
 import numpy as np
-x = [ 1,2,3,4,5,6,7,8]
-y = [ 2,5,1,7,9,3,6,10]
 
 x_arr = np.array(x)
 y_arr = np.array(y)
 
-A = np.vstack([x,np.ones(len(x_arr))]).T
+A = np.vstack((x,np.ones(len(x_arr)))).T
 
 y_arr = y_arr[ :, np.newaxis]
 
-alpha = np.dot(np.dot(np.linalg.inv(np.dot(A.T, A)),A.T),y_arr)
+alpha = np.dot(np.linalg.pinv(A),y_arr)
+print(alpha)
 
-plt.plot(x_arr,y_arr, 'bo')
-plt.plot(x_arr, alpha[0] * x_arr + alpha[1], 'r')
+plt.plot(x_arr,y_arr,'bo')
+plt.plot(x_arr,alpha[0] * x_arr + alpha[1], 'r')
 plt.show()
