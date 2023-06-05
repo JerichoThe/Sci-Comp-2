27d015db53d3e8f5a124318e2826333699a4c661
Katarisu <114144185+JerichoThe@users.noreply.github.com>
2023-06-05 20:48:34 +0700

    Gauss Seidel

--- Gauss-seidel.py
+++ Gauss-seidel.py
@@ -1,44 +1,64 @@
+	
 import numpy as np
+
 x = [
-    [10,4,5],
-    [1,6,1],
-    [3,1,6]
+    [
+        [4, 2, 1],
+        [2, 6, -3],
+        [-1, 2, 7]
+    ],
+    [
+        [80, 6, 12],
+        [5, 160, -7],
+	    [1, 10, 180]
+    ],
+    [
+        [6,1,2],
+        [-2,4,1],
+        [1,2,8]
+    ]
+]
+y = [
+    [8,3,1],
+    [1500,1200,1000],
+    [12,5,10]
 ]
 
-y = [8,5,10]
 
-def gauss_seidel(x_list, y_list, e = 0.01, n = 15):
-  x_arr = np.array(x)
-  y_arr = np.array(y)
+def gauss_seidel(x_list,y_list,e=0.001,n=20):
+    x_arr = np.array(x_list)
+    y_arr = np.array(y_list)
 
-  #check
-  diagonals = np.array(np.diag(x_arr))
-  np.fill_diagonal(x_arr,0)
-  not_diagonals = np.array(np.sum(np.abs(x_arr),1))
+    diagonals = np.array(np.diag(x_arr))
+    np.fill_diagonal(x_arr,0)
+    not_diagonals = np.array(np.sum(np.abs(x_arr),1))
 
-  if(np.all(diagonals < not_diagonals)):
-    print("Not Diagonally Dominant")
-    return False
-  
-  arr_old = np.zeros(np.shape(x_list[0]))
-  x_arr = -x_arr
+    if(np.all(diagonals < not_diagonals)):
+        print("Not Diagonally Dominant")
+        return False
 
-  for i in range(n):
-    arr_new = np.array(arr_old)
-    print("iter",i)
-    print("array", arr_new)
+    arr_old = np.zeros(np.shape(x_list[0]))
+    x_arr = -x_arr
 
-    for j, row in enumerate(x_arr):
-      arr_new[j] = (y_arr[j]+np.dot(row, arr_new))/diagonals[j]
+    for i in range(n):
+        arr_new = np.array(arr_old)
+        
 
-      # check
-      gap = arr_new - arr_old
-      euclid = np.sqrt(np.dot(gap,gap))
+        for j, row in enumerate(x_arr):
+            arr_new[j] = (y_arr[j]+np.dot(row,arr_new))/diagonals[j]
 
-    if(euclid < e):
-      print("Solved")
-      return True
+        gap = arr_new - arr_old
+        euclid = np.sqrt(np.dot(gap,gap))
+        print("Iter", i+1)
+        print("Array", arr_new)
 
-    arr_old = arr_new
+        if(euclid < e):
+            print("Solved")
+            return True
 
-gauss_seidel(x,y)
+        arr_old = arr_new
+
+
+for a,b in zip(x,y):
+    print(f"X : {a}, Y : {b}")
+    gauss_seidel(a,b)
